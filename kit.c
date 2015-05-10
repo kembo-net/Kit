59e2a272ce5b11a6a5941ad5c69f0de16bfb7207
kembo-net <n.kembo+@gmail.com>
2015-05-11 01:38:17 +0900

    コマンド名の変更とリファクタリング

--- kit.c
+++ kit.c
@@ -29,7 +29,7 @@ int getch( ) {
 //文字列を渡すと該当するコマンドナンバーを返す
 //該当するコマンドが無い場合は-1を返す
 int detect_command(const char string[]) {
-  const char CommandList[][8] = { "-v", "init", "commit" };
+  const char CommandList[][8] = { "-v", "init", "do" };
   const int CommandNum = sizeof CommandList / sizeof CommandList[0];
   int i;
   for (i=0; i < CommandNum; i++) {
@@ -38,7 +38,8 @@ int detect_command(const char string[]) {
   if (i == CommandNum) return -1;
   return i;
 }
-void search_dir_opt(
+//オプション列からディレクトリ名(ハイフンついてない項目)を探して返す
+void search_no_hyphen(
     const char *argv[], 
     const int starting_point,
     const int ending_point,
@@ -53,19 +54,57 @@ void search_dir_opt(
       }
     }
   }
-  if (result[0] == '\0') { result = "."; }
-  strcat(result, "/");
+}
+//kitファイルの内容を一行ずつ配列に詰め込んで行数と共に返す
+int read_kit_file(int length, char result[][128]) {
+  int i;
+  char *ret;
+  FILE *fp;
+  fp = fopen(KitFile, "r");
+  if (fp == NULL) {
+    printf("%s is not found.\n", KitFile);
+    exit(1);
+  }
+  for (i = 0; i < length; i++) {
+    if (fgets(result[i], sizeof(result[i]), fp) == NULL) {
+      break;
+    }
+    printf("%s", result[i]);
+  }
+  fclose(fp);
+  return i;
+}
+//配列の中身を指定されたindexまでkitファイルに詰め込んで保存する(空行はスキップ)
+void save_kit_file(int index, char inputs[][128]) {
+  const char br[] = "\n";
+  int i;
+  FILE *fp;
+  fp = fopen(KitFile, "w");
+  if (fp != NULL) {
+    for (i=0; i < index; i++) {
+      if ((inputs[i][0] != '\0') && (inputs[i][0] != '\n')) {
+        fputs(inputs[i], fp);
+        if (inputs[i][strlen(inputs[i]) - 1] != '\n') {
+          fputs(br, fp);
+        }
+      }
+    }
+    fclose(fp);
+  }
 }
 
 void cmd_init(int argc, const char * argv[], char opts[]){
+  const char InitCmd[] = "git init";
   char cmd_str[256], dir_str[128];
   FILE *fp;
   //Gitディレクトリが見つからなかったらgit initする
   if (!opendir(GitDir)) {
-    strcpy(cmd_str, GitCmd);
+    strcpy(cmd_str, InitCmd);
     strcat(cmd_str, opts);
     system(cmd_str);
-    search_dir_opt(argv, 2, argc, dir_str);
+    search_no_hyphen(argv, 2, argc, dir_str);
+    if (dir_str[0] == '\0') { strcpy(dir_str, "."); }
+    strcat(dir_str, "/");
   }
   else { strcpy(dir_str , "./"); }
   //kitファイルの作成
@@ -74,32 +113,28 @@ void cmd_init(int argc, const char * argv[], char opts[]){
   if (fp == NULL) { fp = fopen(dir_str, "w"); }
   fclose(fp);
 }
-void cmd_commit(char opts[]) {
-  const char br[] = "\n";
+void cmd_do(char opts[]) {
   char buffer[128][128] = { {0} };
-  int i, j;
-  FILE *fp;
+  int num;
+  strcpy(buffer[0], opts);
   //ファイルを開いて全部読む
-  fp = fopen(KitFile, "r");
-  if (fp == NULL) {
-    printf("%s is not found.", KitFile);
-    exit(1);
+  num = read_kit_file(127, buffer + 1);
+  //ファイルを一旦リセットして先頭に新しいコメントを追加して全部書き込む
+  save_kit_file(num + 1, buffer);
+}
+void cmd_done(int argc, const char *argv[]) {
+  const char CommitCmd[] = "git commit -am";
+  const char br[] = "\n";
+  char cmd_str[256];
+  int i, j, pointer;
+  FILE *fp;
+  //引数で指定のない場合には先頭の予定を実行する
+  if (argc > 2){
+    pointer = atoi(argv[2]);
   }
-  else {
-    i = 0;
-    while((fgets(buffer[i], 128, fp) != NULL) || (i < 127)) { i++; }
-    fclose(fp);
-  }
-  //ファイルを一旦リセットして先頭に新しいコマンドを追加して全部書き込む
-  fp = fopen(KitFile, "w");
-  if (fp != NULL) {
-    fputs(opts, fp);
-    for (j = 0; ((buffer[j][0] != 0) && (j < i)); j++) {
-      fputs(br, fp);
-      fputs(buffer[j], fp);
-    }
-    fclose(fp);
-  }
+  else { pointer = 0; }
+  //ファイルを開いて全部読む
+  //ファイルを一旦リセットして指定の行以外を全部書き込む
 }
 
 int main(int argc, const char * argv[]) {
@@ -108,7 +143,7 @@ int main(int argc, const char * argv[]) {
   if (argc > 1) {
     command_id = detect_command(argv[1]);
     if (argc > 2) {
-      for (i = 1; i < argc; i++) {
+      for (i = 2; i < argc; i++) {
         strcat(opts, " ");
         strcat(opts, argv[i]);
       }
@@ -124,8 +159,12 @@ int main(int argc, const char * argv[]) {
     case 1://Run init
       cmd_init(argc, argv, opts);
       break;
-    case 2://Run commit
-      cmd_commit(opts);
+    case 2://Run do
+      search_no_hyphen(argv, 2, argc, opts);
+      cmd_do(opts);
+      break;
+    case 3://Run done
+      cmd_done(argc, argv);
       break;
     default:
       printf("unknown command!\n");
