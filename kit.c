83361d1d27588b9029d916653cd261ad52215171
kembo-net <n.kembo+@gmail.com>
2015-05-10 16:57:40 +0900

    initコマンドの実装

--- kit.c
+++ kit.c
@@ -4,10 +4,11 @@
     無ければgit initを実行するか聞く
  */
 #include <stdio.h>
+#include <string.h>
 #include <dirent.h>
 #include <termios.h>
 #include <unistd.h>
-const char KitVersion[] = "Kit 0.0.1";
+const char KitVersion[] = "Kit 0.0.2";
 
 //一文字だけ入力を受け取る
 int getch( ) {
@@ -23,7 +24,7 @@ int getch( ) {
 }
 //文字列を渡すと該当するコマンドナンバーを返す
 //該当するコマンドが無い場合は-1を返す
-int detect_command(char string[]) {
+int detect_command(const char string[]) {
   const char CommandList[][5] = { "-v", "init" };
   const int CommandNum = sizeof CommandList / sizeof CommandList[0];
   int i;
@@ -33,13 +34,32 @@ int detect_command(char string[]) {
   if (i == CommandNum) return -1;
   return i;
 }
+void search_dir_opt(
+    const char *argv[], 
+    const int starting_point,
+    const int ending_point,
+    char result[]) {
+  int i;
+  strcpy(result, "");
+  if (starting_point < ending_point) {
+    for (i = starting_point; i < ending_point; i++) {
+      if (argv[i][0] != '-' && argv[i][0] != '\0') {
+        strcpy(result, argv[i]);
+        break;
+      }
+    }
+  }
+  if (result[0] == '\0') { result = "."; }
+  strcat(result, "/");
+}
 
 int main(int argc, const char * argv[]) {
-  const char GitDir[] = ".git";
-  char init_cmd[256] = "git init";
-  char pname[248];
-  int input_chr;
-  int command_id;
+  const char GitDir[]  = ".git";
+  const char KitFile[] = ".kitstack";
+  char git_cmd[256] = "git";
+  char opts[253], dir_str[128];
+  int i, input_chr, command_id;
+  FILE *fp;
   if (argc > 1) {
     command_id = detect_command(argv[1]);
   }
@@ -52,20 +72,20 @@ int main(int argc, const char * argv[]) {
       break;
     case 1://Run init
       if (!opendir(GitDir)) {
-        //この辺の英文おかしくないだろうか
-        //ものすごく不安だ
-        printf("Not a git repository.\n");
-        printf("Do you run command \"git init\"?(y/n)\n");
-        input_chr = getch();
-        if (input_chr == 'y' || input_chr == 'Y') {
-          printf("Input some options.\n");
-          fgets(pname, sizeof(pname), stdin);
-          pname[strlen(pname) - 1] = '\0';
-          system(strcat(init_cmd, pname));
+        opts[0] = '\0';
+        for (i = 1; i < argc; i++) {
+          strcat(opts, " ");
+          strcat(opts, argv[i]);
         }
-        else { return 0; }
-        //ここから先　今書いてます
+        strcat(git_cmd, opts);
+        system(git_cmd);
+        search_dir_opt(argv, 2, argc, dir_str);
       }
+      else { strcpy(dir_str , "./"); }
+      strcat(dir_str, KitFile);
+      fp = fopen(dir_str, "r");
+      if (fp == NULL) { fp = fopen(dir_str, "w"); }
+      fclose(fp);
       break;
     default:
       printf("unknown command!\n");
@@ -73,3 +93,4 @@ int main(int argc, const char * argv[]) {
   }
   return 0;
 }
+
